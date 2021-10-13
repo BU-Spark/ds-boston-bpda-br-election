321655e8225599164db906164655ddf13afa5375
Wiley Hunt <whunt@bu.edu>
2021-10-13 09:54:00 -0400

    In-progress country_match review

--- src/data-cleaning/country_match.py
+++ src/data-cleaning/country_match.py
@@ -36,7 +36,7 @@ country_code_map = {
     29327 : (11444, "PARAGUAI"),
 
     #Maps code for ASTANA-KAZA (Astana, Kazakhstan)
-    #Note: there is no prior country code for kazakhstan, so leaving as is
+    #Note: there is no prior country code for kazakhstan, so left region code and updated country
     39241 : (39241, "CAZAQUISTÃO"),
 
     #Maps code for ATENAS-GREC (Athens, Greece)
@@ -75,18 +75,164 @@ country_code_map = {
     29416 : (11266, "ESTADOS UNIDOS"),
 
     #Maps code for  BRATISLAVA (Bratislava, Slovakia)
-    #Note, in pre-2010 datasets, this is part of Czechoslovakia, so left with region code
+    #Note, in pre-2010 datasets, this is part of Czechoslovakia, so left region code and updated country
     39209 : (39209, "ESLOVAKIA"),
 
+    #Maps code for  BRAZZAVILLE (Brazzaville, Congo)
+    #Note, in pre-2010 datasets, this entry does not exist, so left region code and updated country
+    39187 : (39187, "CONGO"),
 
-    #Stopped at 4614 in sorted dataset BRAZZAVILLE
-    
-    
-    29793 : (29025, "HONG KONG"),
+    #Maps code for  BRIDGETOWN (Bridgetown, Barbados)
+    29424 : (98663, "BARBADOS"),
 
+    #Maps code for BRUXELAS (Brussels, Belgium)
+    29432 : (98922, "BELGICA"),
+
+    #Maps code for BUCARESTE (Bucarest, Romania)
+    29440 : (98647, "ROMENIA"),
+
+    #Maps code for BUDAPESTE (Budapest, Hungary)
+    29459 : (29238, "HUNGRIA"),
+
+    #Maps code for BUENOS AIRES (Buenos Aires, Argentina)
+    29467 : (11142, "ARGENTINA"),
+
+    #Maps code for CORDOBA (Cordoba, Argentina)
+    29602 : (11142, "ARGENTINA"),
+
+    #Maps code for CAIENA (Cayenne, French Guiana)
+    29475 : (11320, "GUIANA FRANCESA"),
+
+    #Maps code for CAIRO (Cairo, Egypt)
+    29483 : (98086, "EGITO"),
+
+    #Maps code for CAMBERRA (Canberra, Australia)
+    29491 : (98981, "AUSTRALIA"),
+
+    #Maps code for CANTAO (Guangzhou, China)
+    30651 : (98540, "CHINA"),
+
+    #Maps code for CARACAS (Caracas, Venezuela)
+    29505 : (11584, "VENEZUELA"),
+
+    #Maps code for CHICAGO (Chicago, USA)
+    29513 : (11266, "ESTADOS UNIDOS"),
+
+    #Maps code for CHUY (Chuy, Uruguay)
+    29521 : (11568, "URUGUAI"),
+
+    #Maps code for CIDADE DO CABO (Cape Town, South Africa)
+    29530 : (98787, "AFRICA DO SUL"),
+
+    #Maps code for CIUDAD DEL ESTE (Ciudad del Este, Paraguay)
+    29556 : (11444, "PARAGUAI"),
+
+    #Maps code for CIUDAD GUAYANA (Ciudad Guayana, Venezuela)
+    29564 : (11584, "VENEZUELA"),
+
+    #Maps code for COCHABAMBA (Cochabamba, Bolivia)
+    29572 : (11800, "BOLIVIA"),
+
+    #Maps code for COLOMBO (Colombo, Sri Lanka)
+    #Note, in pre-2010 datasets, this entry does not exist, so left region code and updated country
+    30929 : (30929, "SRI LANKA"),
+
+    #Maps code for CONACRI (Conakry, Guinea)
+    #Note, in pre-2010 datasets, this entry does not exist, so left region code and updated country
+    38903 : (38903, "GUINE"),
+
+    #Maps code for CONACEPCION (Concepcion, Paraguay)
+    29580 : (11444, "PARAGUAI"),
+
+    #Maps code for COPENHAGUE-DINA (Copehhagen, Denmark)
+    29599 : (98485, "DINAMARCA"),
+
+    #Maps code for COTONOU (Cotonou, Benin)
+    #Note, in pre-2010 datasets, this entry does not exist, so left region code and updated country
+    38920 : (38920, "BENIM"),
+
+    #Maps code for DILI (Dili, Timor-Leste)
+    #Note, in pre-2010 datasets, this entry does not exist, so left region code and updated country
+    29645 : (29645, "TIMOR-LESTE"),
+
+    #Maps code for DACAR (Dakar, Senegal)
+    29610 : (98248, "SENEGAL"),
+
+    #Maps code for DAMASCO (Damascus, Syria)
+    29637 : (98221, "SIRIA"),
+
+    #Maps code for DAR ES SALAAM (Dar ese Salaam, Tanzania)
+    #Note, in pre-2010 datasets, this entry does not exist, so left region code and updated country
+    38962 : (38962, "TANZANIA"),
+
+    #Maps code for DOHA (Doha, Qatar)
+    #Note, in pre-2010 datasets, this entry does not exist, so left region code and updated country
+    29653 : (29653, "CATAR"),
+
+    #Maps code for DUBLIN (Dublin, Ireland)
     29661 : (29033, "IRLANDA"),
 
+    #Maps code for ENCARNACION (Encarnacion, Paraguay)
+    29670 : (11444, "PARAGUAI"),
+
+    #Maps code for ESTOCOLMO (Stockholm, Sweden)
+    29688 : (98744, "SUECIA"),
+
+    #Maps code for FARO (Faro, Portugual)
+    30961 : (11509, "PORTUGAL"),
+
+    #Maps code for FRANKFURT (Frankfurt, Germany)
+    29696 : (11100, "ALEMANHA"),
+
+    #Maps code for FREETOWN (Freetown, Sierra Leone)
+    #Note, in pre-2010 datasets, this entry does not exist, so left region code and updated country
+    99406 : (99406, "SERRA LEOA"),
+
+    #Maps code for GABORONE (Gaborone, Botswana)
+    #Note, in pre-2010 datasets, this entry does not exist, so left region code and updated country
+    30669 : (30669, "BOTSWANA"),
+
+    #Maps code for GENEBRA (Geneva, Switzerland)
+    29700 : (11525, "SUICA"),
+
+    #Maps code for GEORGETOWN (Georgetown, GUYANA)
+    29718 : (11304, "GUIANA"),
+
+    #Maps code for GUATEMALA (Guatemala) - No-op
+    98000 : (98000, "GUATEMALA"),
+
+    #Maps code for HAMMATSU (Hammatsu, Japan)
     29742 : (11380, "JAPÃO"),
+
+    #Maps code for HANOI (Hanoi, Vietnam)
+    29750 : (2800, "VIETNA"),
+
+    #Maps code for HARARE (Harare, Zimbabwe)
+    29769 : (29092, "ZIMBABWE"),
+
+    #Maps code for HARTFORD (Hartford, CT, USA)
+    30902 : (11266, "ESTADOS UNIDOS"),
+
+    #Maps code for HAVANA (Havana, Cuba)
+    29777 : (98868, "CUBA"),
+
+    #Maps code for HELSINQUE (Helsinki, Finland)
+    29785 : (98426, "FINLANDIA"),
+
+    #Maps code for HONG KONG-HONG (Hong Kong)
+    29793 : (29025, "HONG KONG"),
+
+    #Maps code for HOUSTON (Houston, TX, USA)
+    29807 : (11266, "ESTADOS UNIDOS"),
+
+    #Pause review at line 13385 of sorted (by municipality name) 2018 data (end of HOUSTON)
+
+
+
+
+    
+    
+
     30139 : (11380, "JAPÃO"),
     30198 : (11380, "JAPÃO"),
     30236 : (11380, "JAPÃO"),
@@ -116,8 +262,6 @@ country_code_map = {
 
     30350 : (98566, "CHECOSLOVÁQUIA"),
 
-    29696 : (11100, "ALEMANHA"),
-
     30180 : (11100, "ALEMANHA"),
 
     29890 : (29130, "MALASIA"),
@@ -126,27 +270,17 @@ country_code_map = {
 
     30163 : (98760, "RUSSIA"),
 
-    29432 : (98922, "BELGICA"),
-
     29092 : (29092, "ZIMBABWE"),
 
     30104 : (11428, "MEXICO"),
 
     30279 : (11541, "SURINAME"),
 
-    29505 : (11584, "VENEZUELA"),
-
-    29564 : (11584, "VENEZUELA"),
-
     11800 : (11800, "BOLIVIA"),
-
-    29572 : (11800, "BOLIVIA"),
 
     29904 : (11800, "BOLIVIA"),
 
     30473 : (11800, "BOLIVIA"),
-
-    29599 : (98485, "DINAMARCA"),
 
     30260 : (98329, "PANAMA"),
 
@@ -155,12 +289,6 @@ country_code_map = {
     29912 : (98361, "NIGERIA"),
 
     98361 : (98361, "NIGERIA"),
-
-    29556 : (11444, "PARAGUAI"),
-
-    29580 : (11444, "PARAGUAI"),
-
-    29670 : (11444, "PARAGUAI"),
 
     30309 : (11444, "PARAGUAI"),
 
@@ -178,55 +306,15 @@ country_code_map = {
 
     30325 : (98140, "TRINIDAD TOBAGO"),
 
-    29424 : (98663, "BARBADOS"),
-
-    29440 : (98647, "ROMENIA"),
-
-    29459 : (29238, "HUNGRIA"),
-
-    29467 : (11142, "ARGENTINA"),
-
-    29475 : (11320, "GUIANA FRANCESA"),
-
-    29483 : (98086, "EGITO"),
-
-    29491 : (98981, "AUSTRALIA"),
-
-    29513 : (11266, "ESTADOS UNIDOS"),
-
-    29530 : (98787, "AFRICA DO SUL"),
-
     29548 : (98523, "CINGAPURA"),
 
     29580 : (98485, "DINAMARCA"),
 
-    29602 : (11142, "ARGENTINA"),
-
-    29610 : (98248, "SENEGAL"),
-
-    29637 : (98221, "SIRIA"),
-
-    29688 : (98744, "SUECIA"),
-
     98043 : (98043, "GANA"),
-
-    29700 : (11525, "SUICA"),
-
-    29718 : (11304, "GUIANA"),
 
     29726 : (98000, "GUATEMALA"),
 
     29246 : (29246, "HAITI"),
-
-    29750 : (2800, "VIETNA"),
-
-    30902 : (11266, "ESTADOS UNIDOS"),
-
-    29777 : (98868, "CUBA"),
-
-    29785 : (98426, "FINLANDIA"),
-
-    29807 : (11266, "ESTADOS UNIDOS"),
 
     29840 : (29211, "INDONESIA"),
 
