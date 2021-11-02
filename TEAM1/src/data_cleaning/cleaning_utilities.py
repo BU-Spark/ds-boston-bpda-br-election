da094d6fef927c3bdf76e0d8c8dfb0fbf0a0a085
Wiley Hunt <whunt@bu.edu>
2021-11-02 14:05:58 -0400

    Fixed remove accennt function to work on generic strings

--- TEAM1/src/data_cleaning/cleaning_utilities.py
+++ TEAM1/src/data_cleaning/cleaning_utilities.py
@@ -1,13 +1,24 @@
 
-def remove_accents(row):
+# def remove_accents(row):
+#     '''
+#     Removes accents from the municipallity name text data
+#     :param row - A dataframe row
+#     :return The name with the accent removed
+#     '''
+#     #Source - https://pypi.org/project/Unidecode/
+#     from unidecode import unidecode
+#     remove_accent =unidecode(row.municipality_name)
+#     return remove_accent
+
+def remove_accents(word):
     '''
-    Removes accents from the municipallity name text data
-    :param row - A dataframe row
+    Removes accents from text data
+    :param word - A string from which accents should be removed
     :return The name with the accent removed
     '''
     #Source - https://pypi.org/project/Unidecode/
     from unidecode import unidecode
-    remove_accent =unidecode(row.municipality_name)
+    remove_accent =unidecode(word)
     return remove_accent
 
 def remove_accents_education(row):
