37db527537e84de5f54b3d73479d7fa5cb6f6823
whunt1965 <55954723+whunt1965@users.noreply.github.com>
2021-11-26 11:16:12 -0500

    Cleaned code files and added code description markdown file

--- TEAM1/src/data_cleaning/cleaning_utilities.py
+++ TEAM1/src/data_cleaning/cleaning_utilities.py
@@ -1,15 +1,3 @@
-
-# def remove_accents(row):
-#     '''
-#     Removes accents from the municipallity name text data
-#     :param row - A dataframe row
-#     :return The name with the accent removed
-#     '''
-#     #Source - https://pypi.org/project/Unidecode/
-#     from unidecode import unidecode
-#     remove_accent =unidecode(row.municipality_name)
-#     return remove_accent
-
 def remove_accents(word):
     '''
     Removes accents from text data
@@ -20,9 +8,3 @@ def remove_accents(word):
     from unidecode import unidecode
     remove_accent =unidecode(word)
     return remove_accent
-
-def remove_accents_education(row):
-    from unidecode import unidecode
-    remove_accent =unidecode(row.educational_attainment_description)
-    return remove_accent
-    
--- TEAM1/src/data_cleaning/education_codes.py
+++ TEAM1/src/data_cleaning/education_codes.py
@@ -1,24 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[35]:
-
-
 import pandas as pd
 import numpy as np
 
-
-# In[36]:
-
-
 def read_data_post2006(filename):
     """
-    :function: 
-    reads in csv file and returns dataframe 
-    :params:
-    filename - filepath for dataset that is 2010 or after.
-    :returns:
-    dataframe
+    :function: reads in csv file and returns dataframe 
+    :params: filename - filepath for dataset that is 2010 or after.
+    :returns: dataframe
     """
     data = pd.read_csv(filename)
     data = pd.DataFrame(data)
@@ -28,20 +18,11 @@ def read_data_post2006(filename):
     
     return data
     
-    
-
-
-# In[41]:
-
-
 def read_data_pre2010(filename):
     """
-    :function: 
-    reads in csv file and returns dataframe 
-    :params:
-    filename - filepath for dataset that is before 2010
-    :returns:
-    dataframe
+    :function: reads in csv file and returns dataframe 
+    :params: filename - filepath for dataset that is before 2010
+    :returns:dataframe
     """
     data = pd.read_csv(filename)
     data = pd.DataFrame(data)
@@ -55,12 +36,12 @@ def read_data_pre2010(filename):
     return data
     
 
-
-# In[61]:
-
 def clean2018(df):
-    """the 2018 data treats the characters with accents incorrectly, and thus doesn't match
-    the data of the rest. this function will fix it so the accents are treated correctly."""
+    """
+    :function: the 2018 data treats the characters with accents incorrectly, and thus doesn't match
+               the data of the rest. this function will fix it so the accents are treated correctly.
+    :param: df - A pandas dataframe containing the 2018 Electorate data
+    """
     unique_descriptions = df["educational_attainment_description"].unique()
     for row in range(len(df)):
         if df["educational_attainment_description"][row] == 'ENSINO M<c9>DIO COMPLETO':
@@ -77,18 +58,13 @@ def clean2018(df):
             df.loc[row, "educational_attainment_description"] = 'NAO INFORMADO'
     
     
-
-
 def add_education_codes(data_2002, data):
     """
-    :function: 
-    Adds a column to the dataframe for "data" called educational attainment codes, using the
+    :function: Adds a column to the dataframe for "data" called educational attainment codes, using the
     codes from 2002.
-    :params:
-    data_2002 - data from electorate in 2002, dataframe
-    data - data that needs to have educational attainment codes added, dataframe
-    :returns:
-    df 
+    :params:data_2002 - data from electorate in 2002, dataframe
+    :data: - data that needs to have educational attainment codes added, dataframe
+    :returns: df 
     """
     
     #Get a unique list of the educational attainment codes.
@@ -97,7 +73,7 @@ def add_education_codes(data_2002, data):
     #Get a unique list of the educational attainment description.
     unique_descriptions = data_2002["educational_attainment_description"].unique()
 
-    #Creat list of educationl attainment codes, where each entry is a code for a row of the data
+    #Create list of educationl attainment codes, where each entry is a code for a row of the data
     column_codes = []
     for row in range(len(data)):
         index = np.where(unique_descriptions == data["educational_attainment_description"][row])[0][0]
@@ -106,4 +82,3 @@ def add_education_codes(data_2002, data):
     
     return data
     
-
